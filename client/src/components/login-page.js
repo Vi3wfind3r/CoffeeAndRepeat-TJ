4c33e021977976b01765c173f7e30eda116d62f9
Jamie Wang <sephona@gmail.com>
2017-08-07 16:04:20 -0400

    added skeleton css for login page

--- client/src/components/login-page.js
+++ client/src/components/login-page.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import './login-page.css';
 
 export default class LoginPage extends React.Component {
     
     render(){
         return (
-            <div>
-                <div>
-                    <p>FrenchX</p>
-                    <p>Coffee & Repeat</p>
-                    <p>Learning Coffee and its nuances through repetition!</p>
+            <div className='login-container'>
+                <div className='title-box'>
+                    <p className='company'>FrenchX</p>
+                    <p className='app-name'>Coffee & Repeat</p>
+                    <p className='app-description'>Learning Coffee and its nuances through repetition!</p>
                 </div>
-                <div>
+                <div className='login-button'>
                     <a href={'/api/auth/google'}>Login with Google</a>
                 </div>
             </div>

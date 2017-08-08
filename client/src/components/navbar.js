c11304ba591e9440ceb3223ad6ef5b34596baf7e
Jamie Wang <sephona@gmail.com>
2017-08-08 15:28:24 -0400

    fixed package jsons

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './navbar.css';
+import {Link} from 'react-router-dom';
 
 export default class Navbar extends React.Component {
   
@@ -8,7 +9,9 @@ export default class Navbar extends React.Component {
       <nav>
         <ul className='navbar'>
           <li>Hello !</li>
-          <li>Logout</li>
+          <Link to='/api/auth/logout'>
+            <li>Logout</li>
+          </Link>
         </ul>
       </nav>
     );

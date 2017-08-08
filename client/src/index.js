f039fa9b9a787213d281300fab90e7280a5087fe
Jamie Wang <sephona@gmail.com>
2017-08-08 15:09:33 -0400

    install react router and react router dom and set up browserrouter

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 import './index.css';
 
-// import {Provider} from 'react-redux';
-// import store from './store';
+import {Provider} from 'react-redux';
+import store from './store';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 );

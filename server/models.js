770aa260c0b3761590a77e6a0f2037e8f2a86bb0
tannaurus <contacttannergill@gmail.com>
2017-08-08 11:28:41 -0700

    Rigged the BearerStrat up to the database so it checks the actual database instead of the mock one.

--- server/models.js
+++ server/models.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
   firstName: {type: String, required: true},
   lastName: {type: String, required: true},
-  id: {type: String, required: true}
+  id: {type: String, required: true},
+  token: {type: String, required: true}
 });
 
 const Users = mongoose.model('Users', userSchema);

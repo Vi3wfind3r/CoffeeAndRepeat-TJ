f8c6a52dda76a73662bc2f0eb8910deb07dc0516
Jamie Wang <sephona@gmail.com>
2017-08-08 14:10:26 -0400

    corrected typo in index js and models

--- server/index.js
+++ server/index.js
@@ -1,158 +1,3 @@
-// const path = require('path');
-// const express = require('express');
-// const passport = require('passport');
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// const BearerStrategy = require('passport-http-bearer').Strategy;
-// const mongoose = require('mongoose');
-
-// const {User} = require('./models');
-
-// let secret = {
-//   CLIENT_ID: process.env.CLIENT_ID,
-//   CLIENT_SECRET: process.env.CLIENT_SECRET,
-//   DATABASE_URL: process.env.DATABASE_URL
-// };
-
-// if(process.env.NODE_ENV != 'production') {
-//   secret = require('./secret');
-// }
-
-// const app = express();
-
-
-// //const database = { 
-// //};
-
-// app.use(passport.initialize());
-
-// passport.use(
-//     new GoogleStrategy({
-//       clientID:  secret.CLIENT_ID,
-//       clientSecret: secret.CLIENT_SECRET,
-//       callbackURL: '/api/auth/google/callback'
-//     },
-//     (accessToken, refreshToken, profile, cb) => {
-//     User.findOneAndUpdate(
-//           {googleId: profile.id},
-//           {$set: {accessToken: accessToken, googleId: profile.id}},
-//           {upsert: true, new: true}
-//         ).then((user) => {
-//           console.log('updated user', user);
-//           return cb(null, user);
-//         });
-//     }
-// ));
-
-// passport.use(
-//     new BearerStrategy(
-//         (token, done) => {
-//           //check to see what the token is
-//           console.log('about to search for', token);
-//             // Job 3: Update this callback to try to find a user with a
-//             // matching access token.  If they exist, let em in, if not,
-//             // don't.
-
-//             //might want to look into User.findOne ( check to see if the access token matches the token...)
-//           //replace this with a promise that will handle if you find the user
-//           //or catch error if you don't find the user
-//             if (!(token in database)) {
-//             return done(null, false);
-//           }
-//           return done(null, database[token]);
-//         }
-//     )
-// );
-
-
-// app.get('/api/auth/google',
-//     passport.authenticate('google', {scope: ['profile']}));
-
-// app.get('/api/auth/google/callback',
-//     passport.authenticate('google', {
-//         // succesRedirect: '/success',
-//       failureRedirect: '/',
-//       session: false
-//     }),
-//     (req, res) => {
-//       res.cookie('accessToken', req.user.accessToken, {expires: 0});
-//       res.redirect('/');
-//     }
-// );
-
-// // app.get('/success', (req, res) => {
-// //     console.log(req.user);
-// // });
-
-// app.get('/api/auth/logout', (req, res) => {
-//   req.logout();
-//   res.clearCookie('accessToken');
-//   res.redirect('/');
-// });
-
-// app.get('/api/me',
-//     passport.authenticate('bearer', {session: false}),
-//     (req, res) => {
-//       console.log(req.user);
-//       User.find()
-//       .then(user => {
-//         console.log(user);
-//       });
-// });
-
-
-// app.get('/api/questions',
-//     passport.authenticate('bearer', {session: false}),
-//     (req, res) => res.json(['Question 1', 'Question 2'])
-// );
-
-// // Serve the built client
-// app.use(express.static(path.resolve(__dirname, '../client/build')));
-
-// // Unhandled requests which aren't for the API should serve index.html so
-// // client-side routing using browserHistory can function
-// app.get(/^(?!\/api(\/|$))/, (req, res) => {
-//   const index = path.resolve(__dirname, '../client/build', 'index.html');
-//   res.sendFile(index);
-// });
-
-// let server;
-// function runServer(port=3001) {//4000???
-//   return new Promise((resolve, reject) => {
-//     mongoose.connect(secret.DATABASE_URL, err => {
-//       if (err) {
-//         return reject(err);
-//       }
-
-//       server = app.listen(port, () => {
-//         console.log(`Your app is listening on ${port}`);
-//         resolve();
-//       }).on('error', reject);
-//     });
-//   });
-// }
-
-// function closeServer() {
-//   //close the database here
-//   return mongoose.disconnect().then(() => {
-//     return new Promise((resolve, reject) => {
-//       server.close(err => {
-//         if (err) {
-//           return reject(err);
-//         }
-//         resolve();
-//       });
-//     });
-//   });
-// }
-
-// if (require.main === module) {
-//   runServer();
-// }
-
-// module.exports = {
-//   app, runServer, closeServer
-// };
-
 const path = require('path');
 const express = require('express');
 const passport = require('passport');
@@ -160,7 +5,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const BearerStrategy = require('passport-http-bearer').Strategy;
 const mongoose = require('mongoose');
 
-const {Users} = require('./models');
+const {User} = require('./models');
 
 let secret = {
   CLIENT_ID: process.env.CLIENT_ID,
@@ -229,9 +74,9 @@ app.get('/api/auth/google/callback',
     (req, res) => {
       res.cookie('accessToken', req.user.accessToken, {expires: 0});
       res.redirect('/');
-      console.log('####',Users.findOne({id: req.user.googleId}));
-      if(Users.find({id: req.user.googleId}).length === 0){
-        Users.findOneAndUpdate({id: req.user.googleId},
+      console.log('####',User.findOne({id: req.user.googleId}));//need to update to a then
+      if(User.find({id: req.user.googleId}).length === 0){
+        User.findOneAndUpdate({id: req.user.googleId},
           {$set: {
           firstName: req.user.firstName,
           lastName: req.user.lastName,

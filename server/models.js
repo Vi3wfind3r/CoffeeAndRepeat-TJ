5a7115704a274dd5626fd41b2cbbc95bd10f7223
tannaurus <contacttannergill@gmail.com>
2017-08-10 10:34:40 -0700

    Updated the questions model.

--- server/models.js
+++ server/models.js
@@ -1,19 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
-  firstName: {type: String, required: true},
-  lastName: {type: String, required: true},
-  id: {type: String, required: true},
-  token: {type: String, required: true}
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  id: { type: String, required: true },
+  token: { type: String, required: true }
 });
 
 const questionSchema = mongoose.Schema({
-  question: {type: String, required: true},
-  answer: {type: String, required: true}
+  question: { type: String, required: true },
+  answer: { type: String, required: true },
+  questionId: { type: String, required: true }
 });
 
-const Questions = mongoose.model('Questions', questionSchema);
+const Questions = mongoose.model("Questions", questionSchema);
 
-const Users = mongoose.model('Users', userSchema);
+const Users = mongoose.model("Users", userSchema);
 
-module.exports = {Users, Questions};
+module.exports = { Users, Questions };
